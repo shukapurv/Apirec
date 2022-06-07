779e85430fa3720632e2d37c089f90b878be4918
apoorve <apoorve.shukla.cse20@itbhu.ac.in>
2022-06-07 12:47:37 +0530

    Configured fields in userAdmin

--- app/core/admin.py
+++ app/core/admin.py
@@ -11,6 +11,22 @@ class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users."""
     ordering = ['id']
     list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
 
 
 admin.site.register(models.User, UserAdmin)
